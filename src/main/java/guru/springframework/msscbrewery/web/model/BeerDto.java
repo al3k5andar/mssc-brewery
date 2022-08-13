6a3e9fda5e0fb2b98c0dd9d401e6e1a1a1c2b475
al3k5andar <aca.temac@gmail.com>
2022-08-13 22:50:16 +0200

    mapstruct dependency added and mapper created for Beer and Customer classes

--- src/main/java/guru/springframework/msscbrewery/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbrewery/web/model/BeerDto.java
@@ -9,6 +9,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 /**
@@ -31,4 +32,7 @@ public class BeerDto {
 
     @Positive
     private Long upc;
+
+    private OffsetDateTime createdAt;
+    private OffsetDateTime lastModifiedAt;
 }
